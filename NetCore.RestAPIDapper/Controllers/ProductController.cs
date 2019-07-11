0e67697e014cdea7a1206a6b4f6629003d17dc52
buuhq <huynhquocbuu@gmail.com>
2019-07-11 14:11:35 +0700

    Test Ket noi SQL DB dung dapper

---
+++ NetCore.RestAPIDapper/Controllers/ProductController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using NetCore.RestAPIDapper.Models;
+
+namespace NetCore.RestAPIDapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly string _sqlConnectionString;
+        public ProductController(IConfiguration configuration)
+        {
+            _sqlConnectionString = configuration.GetConnectionString("SqlConnectionString");
+        }
+        // GET: api/Product
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get()
+        {
+            using (var conn = new SqlConnection(_sqlConnectionString))
+            {
+                if (conn.State == System.Data.ConnectionState.Closed) { conn.Open(); }
+                string sql = "SELECT Id, Sku, Price, DiscountPrice, IsActive, ImageUrl, ViewCount, CreatedAt FROM Products";
+                var result = await conn.QueryAsync<Product>(sql,null,null,null,System.Data.CommandType.Text);
+                return result;
+            }
+            
+        }
+
+        // GET: api/Product/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Product
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Product/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ NetCore.RestAPIDapper/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCore.RestAPIDapper.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Sku { get; set; }
+        public float Price { get; set; }
+        public float? DiscountPrice { get; set; }
+        public bool IsActive { get; set; }
+        public string ImageUrl { get; set; }
+        public int ViewCount { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
